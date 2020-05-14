daca4a6b4486225630c8b266f07c0ff04d0b8f0a
K Siddartha <siddartha.kopparapu@kroger.com>
2020-05-14 12:48:45 +0530

    Printing raw values

--- NumToIndianString/NumToIndianString/NumberToStringConverter.cs
+++ NumToIndianString/NumToIndianString/NumberToStringConverter.cs
@@ -14,17 +14,51 @@ namespace NumToIndianString
         Crores = 7,
         MoreThanCrores
     }
-    
+
+    public enum RawValues
+    {
+        One = 1,
+        Two = 2,
+        Three = 3,
+        Four = 4,
+        Five = 5,
+        Six = 6,
+        Seven = 7,
+        Eight = 8,
+        Nine = 9,
+        Ten = 10,
+        Eleven = 11,
+        Twelve = 12,
+        Thirteen= 13,
+        Fourteen = 14,
+        Fifteen = 15,
+        Sixteen = 16,
+        Seventeen = 17,
+        Eighteen = 18,
+        Nineteen = 19
+    }
     
     public class NumberToStringConverter
     {
         public PlaceValue GetHighestPlaceValue(string number)
         {
-            double convertedLongInt = Convert.ToDouble(number);
-            int placeValueInt =Convert.ToInt32(Math.Floor(Math.Log10(convertedLongInt)));
+            double convertedLongInt = System.Convert.ToDouble(number);
+            int placeValueInt = System.Convert.ToInt32(Math.Floor(Math.Log10(convertedLongInt)));
             return (PlaceValue) placeValueInt;
         }
-        
-        
+
+        public string Convert(string number)
+        {
+            if (number.Length <= 2 && System.Convert.ToInt16(number) < 20)
+            {
+               return getRawValue(System.Convert.ToInt16(number)).ToString();
+            }
+            return number;
+        }
+
+        private RawValues getRawValue(int number)
+        {
+            return (RawValues) number;
+        }
     }
 }
--- NumToIndianString/NumToIndianStringTest/NumberToStringCoverterTest.cs
+++ NumToIndianString/NumToIndianStringTest/NumberToStringCoverterTest.cs
@@ -13,6 +13,15 @@ namespace NumToIndianStringTest
             var placeValue = converter.GetHighestPlaceValue("200");
             Assert.AreEqual(PlaceValue.Hundreds,placeValue);
         }
+        
+        [TestMethod]
+        public void ShouldGetHigestPlaceValueForPassedInStringForDigits()
+        {
+            var converter = new NumberToStringConverter();
+            var placeValue = converter.GetHighestPlaceValue("9");
+            Assert.AreEqual(PlaceValue.Digits,placeValue);
+        }
+        
         [TestMethod]
         public void ShouldGetGreaterThanCroresOnPassing10Crores()
         {
@@ -27,6 +36,31 @@ namespace NumToIndianStringTest
             var converter = new NumberToStringConverter();
             var placeValue = converter.GetHighestPlaceValue("9999999");
             Assert.AreEqual(PlaceValue.TenLakhs,placeValue);
+        }
+
+        [TestMethod]
+        public void shouldSpellOutRawNumberBasedForHighestValuesTill20()
+        {
+            var converter = new NumberToStringConverter();
+            Assert.AreEqual(converter.Convert("1"), "One");
+            Assert.AreEqual(converter.Convert("2"), "Two");
+            Assert.AreEqual(converter.Convert("3"), "Three");
+            Assert.AreEqual(converter.Convert("4"), "Four");
+            Assert.AreEqual(converter.Convert("5"), "Five");
+            Assert.AreEqual(converter.Convert("6"), "Six");
+            Assert.AreEqual(converter.Convert("7"), "Seven");
+            Assert.AreEqual(converter.Convert("8"), "Eight");
+            Assert.AreEqual(converter.Convert("9"), "Nine");
+            Assert.AreEqual(converter.Convert("10"), "Ten");
+            Assert.AreEqual(converter.Convert("11"), "Eleven");
+            Assert.AreEqual(converter.Convert("12"), "Twelve");
+            Assert.AreEqual(converter.Convert("13"), "Thirteen");
+            Assert.AreEqual(converter.Convert("14"), "Fourteen");
+            Assert.AreEqual(converter.Convert("15"), "Fifteen");
+            Assert.AreEqual(converter.Convert("16"), "Sixteen");
+            Assert.AreEqual(converter.Convert("17"), "Seventeen");
+            Assert.AreEqual(converter.Convert("18"), "Eighteen");
+            Assert.AreEqual(converter.Convert("19"), "Nineteen");
         }
     }
 }
